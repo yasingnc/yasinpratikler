34a4e381f2da184bb9daf416f56f7d84f9947bb3
Yasin Genç <yasingenc91@gmail.com>
2022-01-25 22:32:14 +0300

    Dosyalar Oluşturuldu

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace consoleprogramlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname );
+
+        }
+       
+     }
+}
